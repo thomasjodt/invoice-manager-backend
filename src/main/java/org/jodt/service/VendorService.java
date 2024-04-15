e2019caa79741f52d9fb470e15079ba76d241dc9
thomasjodt <jodt5200@gmail.com>
2024-04-14 22:43:24 -0500

    FEAT(service): Create Vendor service class with CRUD methods

---
+++ src/main/java/org/jodt/service/VendorService.java
@@ -0,0 +1,66 @@
+package org.jodt.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.jodt.entity.Vendor;
+import org.jodt.models.VendorDto;
+import org.jodt.repository.IVendorRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class VendorService implements IVendorService {
+    @Inject
+    IVendorRepository repository;
+
+    @Override
+    public List<Vendor> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public List<Vendor> getAll(Integer page, Integer offset) {
+        return repository.getAll(page, offset);
+    }
+
+    @Override
+    public Optional<Vendor> findById(Long id) {
+        return Optional.ofNullable(repository.findById(id));
+    }
+
+    @Override
+    public Vendor save(Vendor vendor) {
+        return repository.save(vendor);
+    }
+
+    @Override
+    public Vendor update(Vendor vendor) {
+        return repository.update(vendor);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+    @Override
+    public List<VendorDto> findByName(String vendorName) {
+        return repository.findByName('%' + vendorName + '%');
+    }
+
+    @Override
+    public List<VendorDto> getVendorsWithBalance() {
+        return repository.getVendorsWithBalance();
+    }
+
+    @Override
+    public List<VendorDto> getVendorsWithBalance(Integer limit, Integer offset) {
+        return repository.getVendorsWithBalance(limit, offset);
+    }
+
+    @Override
+    public VendorDto getVendorWithBalanceById(Long id) {
+        return repository.getVendorWithBalanceById(id);
+    }
+}
