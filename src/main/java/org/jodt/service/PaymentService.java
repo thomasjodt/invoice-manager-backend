d0e0562c1e8741a4adf06f89cce240e8383f1aa2
thomasjodt <jodt5200@gmail.com>
2024-04-14 22:43:18 -0500

    FEAT(service): Create Payment service class with CRUD methods

---
+++ src/main/java/org/jodt/service/PaymentService.java
@@ -0,0 +1,50 @@
+package org.jodt.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.jodt.entity.Payment;
+import org.jodt.repository.IPaymentRepository;
+import org.jodt.repository.IRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class PaymentService implements IPaymentService {
+    @Inject
+    IPaymentRepository repository;
+
+    @Override
+    public List<Payment> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public List<Payment> getAll(Integer page, Integer offset) {
+        return repository.getAll(page, offset);
+    }
+
+    @Override
+    public Optional<Payment> findById(Long id) {
+        return Optional.ofNullable(repository.findById(id));
+    }
+
+    @Override
+    public Payment save(Payment payment) {
+        return repository.save(payment);
+    }
+
+    @Override
+    public Payment update(Payment payment) {
+        return repository.update(payment);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+    public List<Payment> getPaymentsByInvoiceId(Long id) {
+        return repository.getPaymentsByInvoiceId(id);
+    }
+}
