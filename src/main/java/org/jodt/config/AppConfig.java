50c857aeead0fa2e5145334dfb0835d4941fe712
thomasjodt <jodt5200@gmail.com>
2024-04-14 22:48:12 -0500

    FEAT(config): Create the Application path

---
+++ src/main/java/org/jodt/config/AppConfig.java
@@ -0,0 +1,8 @@
+package org.jodt.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AppConfig extends Application {
+}
