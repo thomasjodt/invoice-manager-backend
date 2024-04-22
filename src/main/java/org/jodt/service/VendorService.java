02d53719538b40c3cc5d79ed9124672d613f04d7
thomasjodt <jodt5200@gmail.com>
2024-04-21 22:56:03 -0500

    REFACTOR(vendors): Create utility methods to implement DRY.

--- src/main/java/org/jodt/service/VendorService.java
+++ src/main/java/org/jodt/service/VendorService.java
@@ -21,122 +21,58 @@ public class VendorService implements IVendorService {
     @Inject
     InvoiceIService invoiceService;
 
-
     @Override
     public ResponseDTO<List<VendorDto>> findByName(String vendorName) {
         ResponseDTO<List<Vendor>> response = repository.findByName('%' + vendorName + '%');
-        List<Vendor> vendors = response.getData();
-        List<VendorDto> list = new ArrayList<>();
-
-        vendors.forEach(vendor -> {
-            ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
-            AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-            AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-            invoices.getData().forEach((invoice) -> {
-                allAmount.updateAndGet(v -> v + invoice.getAmount());
-                invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
-            });
-
-            VendorDto dto = new VendorDto(vendor.getId(), vendor.getName(), vendor.getFullName(), 0D);
-            dto.setBalance(allAmount.get() - allPaid.get());
-
-            list.add(dto);
-        });
-        ResponseDTO<List<VendorDto>> res = new ResponseDTO<>();
-        res.setData(list);
-
-        return res;
+        return getResponseDTO(response);
     }
 
     @Override
     public ResponseDTO<List<VendorDto>> findByName(String vendorName, Integer page, Integer offset) {
         ResponseDTO<List<Vendor>> response = repository.findByName('%' + vendorName + '%', page, offset);
-        List<Vendor> vendors = response.getData();
-        List<VendorDto> list = new ArrayList<>();
-
-        vendors.forEach(vendor -> {
-            ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
-            AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-            AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-            invoices.getData().forEach((invoice) -> {
-                allAmount.updateAndGet(v -> v + invoice.getAmount());
-                invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
-            });
-
-            VendorDto dto = new VendorDto(vendor.getId(), vendor.getName(), vendor.getFullName(), 0D);
-            dto.setBalance(allAmount.get() - allPaid.get());
-
-            list.add(dto);
-        });
-        ResponseDTO<List<VendorDto>> res = new ResponseDTO<>();
-        res.setData(list);
-        res.setCount(response.getCount());
-
-        return res;
+        return getResponseDTO(response);
     }
 
     @Override
     public ResponseDTO<List<VendorDto>> getAll() {
         ResponseDTO<List<Vendor>> response = repository.getAll();
-        List<Vendor> vendors = response.getData();
-        List<VendorDto> list = new ArrayList<>();
-
-        vendors.forEach(vendor -> {
-            ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
-            AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-            AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-            invoices.getData().forEach((invoice) -> {
-                allAmount.updateAndGet(v -> v + invoice.getAmount());
-                invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
-            });
-
-            VendorDto dto = new VendorDto(vendor.getId(), vendor.getName(), vendor.getFullName(), 0D);
-            dto.setBalance(allAmount.get() - allPaid.get());
-
-            list.add(dto);
-        });
-        ResponseDTO<List<VendorDto>> res = new ResponseDTO<>();
-        res.setData(list);
-
-        return res;
+        return getResponseDTO(response);
     }
 
     @Override
     public ResponseDTO<List<VendorDto>> getAll(Integer page, Integer offset) {
         ResponseDTO<List<Vendor>> response = repository.getAll(page, offset);
-        List<Vendor> vendors = response.getData();
-        List<VendorDto> list = new ArrayList<>();
-
-        vendors.forEach(vendor -> {
-            ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
-            AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-            AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-            invoices.getData().forEach((invoice) -> {
-                allAmount.updateAndGet(v -> v + invoice.getAmount());
-                invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
-            });
-
-            VendorDto dto = new VendorDto(vendor.getId(), vendor.getName(), vendor.getFullName(), 0D);
-            dto.setBalance(allAmount.get() - allPaid.get());
-
-            list.add(dto);
-        });
-        ResponseDTO<List<VendorDto>> res = new ResponseDTO<>();
-        res.setData(list);
-        res.setCount(response.getCount());
-
-        return res;
+        return getResponseDTO(response);
     }
 
     @Override
     public Optional<VendorDto> findById(Long id) {
         Vendor vendor = repository.findById(id);
-        ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
+        VendorDto dto = getDto(vendor);
+        return Optional.of(dto);
+    }
 
+    @Override
+    public VendorDto save(VendorDto vendorDto) {
+        Vendor vendor = new Vendor(vendorDto.getId(), vendorDto.getName(), vendorDto.getFullName());
+        Vendor newVendor = repository.save(vendor);
+        return getDto(newVendor);
+    }
+
+    @Override
+    public VendorDto update(VendorDto vendorDto) {
+        Vendor vendor = new Vendor(vendorDto.getId(), vendorDto.getName(), vendorDto.getFullName());
+        Vendor updatedVendor = repository.update(vendor);
+        return getDto(updatedVendor);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+    private VendorDto getDto(Vendor vendor) {
+        ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(vendor.getId());
         AtomicReference<Double> allPaid = new AtomicReference<>(0D);
         AtomicReference<Double> allAmount = new AtomicReference<>(0D);
 
@@ -147,54 +83,19 @@ public class VendorService implements IVendorService {
 
         VendorDto dto = new VendorDto(vendor.getId(), vendor.getName(), vendor.getFullName(), 0D);
         dto.setBalance(allAmount.get() - allPaid.get());
-
-        return Optional.of(dto);
-    }
-
-    @Override
-    public VendorDto save(VendorDto vendorDto) {
-        Vendor vendor = new Vendor(vendorDto.getId(), vendorDto.getName(), vendorDto.getFullName());
-        var newVendor = repository.save(vendor);
-
-        ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(newVendor.getId());
-
-        AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-        AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-        invoices.getData().forEach((invoice) -> {
-            allAmount.updateAndGet(v -> v + invoice.getAmount());
-            invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
-        });
-
-        VendorDto dto = new VendorDto(newVendor.getId(), newVendor.getName(), newVendor.getFullName(), 0D);
-        dto.setBalance(allAmount.get() - allPaid.get());
-
         return dto;
     }
+    private ResponseDTO<List<VendorDto>> getResponseDTO(ResponseDTO<List<Vendor>> response) {
+        List<Vendor> vendors = response.getData();
+        List<VendorDto> list = new ArrayList<>();
 
-    @Override
-    public VendorDto update(VendorDto vendorDto) {
-        Vendor vendor = new Vendor(vendorDto.getId(), vendorDto.getName(), vendorDto.getFullName());
-        Vendor updatedVendor = repository.update(vendor);
-
-        ResponseDTO<List<InvoiceDto>> invoices = invoiceService.getInvoicesByVendorId(updatedVendor.getId());
-
-        AtomicReference<Double> allPaid = new AtomicReference<>(0D);
-        AtomicReference<Double> allAmount = new AtomicReference<>(0D);
-
-        invoices.getData().forEach((invoice) -> {
-            allAmount.updateAndGet(v -> v + invoice.getAmount());
-            invoice.getPayments().forEach(p -> allPaid.updateAndGet(v -> v + p.getAmount()));
+        vendors.forEach(vendor -> {
+            VendorDto dto = getDto(vendor);
+            list.add(dto);
         });
-
-        VendorDto dto = new VendorDto(updatedVendor.getId(), updatedVendor.getName(), updatedVendor.getFullName(), 0D);
-        dto.setBalance(allAmount.get() - allPaid.get());
-
-        return dto;
-    }
-
-    @Override
-    public void delete(Long id) {
-        repository.delete(id);
+        ResponseDTO<List<VendorDto>> res = new ResponseDTO<>();
+        res.setData(list);
+        res.setCount(response.getCount());
+        return res;
     }
 }
