9c45d2f1161d6d6f4dcf4e6c9c80eed22804f03b
thomasjodt <jodt5200@gmail.com>
2024-07-09 13:08:56 -0500

    REFACTOR(entity): Remove unnecessary constructor.

--- src/main/java/org/jodt/entity/Invoice.java
+++ src/main/java/org/jodt/entity/Invoice.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.jodt.models.InvoiceDto;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -29,17 +28,8 @@ public class Invoice implements Serializable {
     protected LocalDate dueDate;
 
     @OneToOne
-    @JoinColumn(name = "vendor_id", referencedColumnName = "id")
+    @JoinColumn(name = "vendor_id")
     protected Vendor vendor;
 
     protected Double amount;
-
-    public Invoice (InvoiceDto invoiceDto) {
-        this.id = invoiceDto.getId();
-        this.invoiceNumber = invoiceDto.getInvoiceNumber();
-        this.emissionDate = invoiceDto.getEmissionDate();
-        this.dueDate = invoiceDto.getDueDate();
-        this.vendor = invoiceDto.getVendor();
-        this.amount = invoiceDto.getAmount();
-    }
 }
