f77609f903c6494b84de667b5df9fa1e357edb74
thomasjodt <jodt5200@gmail.com>
2024-07-09 14:17:43 -0500

    REFACTOR(service): Inject PaymentRepository to get the invoices payments.

--- src/main/java/org/jodt/service/InvoiceService.java
+++ src/main/java/org/jodt/service/InvoiceService.java
@@ -3,9 +3,12 @@ package org.jodt.service;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.jodt.entity.Invoice;
+import org.jodt.entity.Payment;
 import org.jodt.models.ResponseDTO;
+import org.jodt.repository.IPaymentRepository;
 import org.jodt.repository.InvoiceIRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,14 +17,23 @@ public class InvoiceService implements InvoiceIService {
     @Inject
     InvoiceIRepository repository;
 
+    @Inject
+    IPaymentRepository paymentRepository;
+
     @Override
     public ResponseDTO<List<Invoice>> getAll() {
-        return repository.getAll();
+        ResponseDTO<List<Invoice>> invoices =  repository.getAll();
+        List<Invoice> updatedInvoices = updateInvoices(invoices);
+        invoices.setData(updatedInvoices);
+        return invoices;
     }
 
     @Override
     public ResponseDTO<List<Invoice>> getAll(Integer limit, Integer offset) {
-        return repository.getAll(limit, offset);
+        ResponseDTO<List<Invoice>> invoices = repository.getAll(limit, offset);
+        List<Invoice> updatedInvoices = updateInvoices(invoices);
+        invoices.setData(updatedInvoices);
+        return invoices;
     }
 
     @Override
@@ -48,12 +60,28 @@ public class InvoiceService implements InvoiceIService {
 
     @Override
     public ResponseDTO<List<Invoice>> getInvoicesByVendorId(Long id) {
-        return repository.getInvoicesByVendor(id);
+        ResponseDTO<List<Invoice>> invoices = repository.getInvoicesByVendor(id);
+        List<Invoice> updatedInvoices = updateInvoices(invoices);
+        invoices.setData(updatedInvoices);
+        return invoices;
 
     }
 
     @Override
     public ResponseDTO<List<Invoice>> getInvoicesByVendorId(Long id, Integer limit, Integer offset) {
-        return repository.getInvoicesByVendor(id, limit, offset);
+        ResponseDTO<List<Invoice>> invoices =  repository.getInvoicesByVendor(id, limit, offset);
+        List<Invoice> updatedInvoices = updateInvoices(invoices);
+        invoices.setData(updatedInvoices);
+        return invoices;
+    }
+
+    private List<Invoice> updateInvoices(ResponseDTO<List<Invoice>> invoices) {
+        List<Invoice> updatedInvoices = new ArrayList<>();
+        for (Invoice invoice : invoices.getData()) {
+            ResponseDTO<List<Payment>> payments = paymentRepository.getPaymentsByInvoiceId(invoice.getId());
+            invoice.setPayments(payments.getData());
+            updatedInvoices.add(invoice);
+        }
+        return updatedInvoices;
     }
 }
